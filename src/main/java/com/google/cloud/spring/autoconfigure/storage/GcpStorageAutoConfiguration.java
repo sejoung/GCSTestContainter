5325ca71edf61a83456a0c2c7da118ba3533950f
sejoung kim <sejoung@gmail.com>
2026-03-04 14:04:38 +0900

    GcpStorageAutoConfiguration 수정 방향대로 수정

---
+++ src/main/java/com/google/cloud/spring/autoconfigure/storage/GcpStorageAutoConfiguration.java
@@ -0,0 +1,103 @@
+package com.google.cloud.spring.autoconfigure.storage;
+
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.cloud.spring.autoconfigure.core.GcpProperties;
+import com.google.cloud.spring.core.DefaultCredentialsProvider;
+import com.google.cloud.spring.core.GcpProjectIdProvider;
+import com.google.cloud.spring.core.UserAgentHeaderProvider;
+import com.google.cloud.spring.storage.GoogleStorageProtocolResolver;
+import com.google.cloud.spring.storage.GoogleStorageProtocolResolverSettings;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * An auto-configuration for Google {@link GoogleStorageProtocolResolverSettings} bean definition.
+ * Also it {@link Import} a {@link GoogleStorageProtocolResolver} to register it with the {@code
+ * DefaultResourceLoader}.
+ *
+ * @see GoogleStorageProtocolResolver
+ */
+@AutoConfiguration
+@ConditionalOnClass({GoogleStorageProtocolResolverSettings.class, Storage.class})
+@ConditionalOnProperty(value = "spring.cloud.gcp.storage.enabled", matchIfMissing = true)
+@EnableConfigurationProperties({GcpProperties.class, GcpStorageProperties.class})
+@Import(GoogleStorageProtocolResolver.class)
+public class GcpStorageAutoConfiguration { // NOSONAR squid:S1610 must be a class for Spring
+
+    private final GcpProjectIdProvider gcpProjectIdProvider;
+
+    private final CredentialsProvider credentialsProvider;
+
+    private final String universeDomain;
+
+    private final String host;
+
+    public GcpStorageAutoConfiguration(
+            GcpProjectIdProvider coreProjectIdProvider,
+            CredentialsProvider credentialsProvider,
+            GcpStorageProperties gcpStorageProperties)
+            throws IOException {
+
+        this.gcpProjectIdProvider =
+                gcpStorageProperties.getProjectId() != null
+                        ? gcpStorageProperties::getProjectId
+                        : coreProjectIdProvider;
+
+        this.credentialsProvider =
+                gcpStorageProperties.getCredentials().hasKey()
+                        ? new DefaultCredentialsProvider(gcpStorageProperties)
+                        : credentialsProvider;
+
+        this.universeDomain = gcpStorageProperties.getUniverseDomain();
+        this.host = gcpStorageProperties.getHost();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Storage storage() throws IOException {
+        StorageOptions.Builder storageOptionsBuilder =
+                StorageOptions.newBuilder()
+                        .setHeaderProvider(new UserAgentHeaderProvider(GcpStorageAutoConfiguration.class))
+                        .setProjectId(this.gcpProjectIdProvider.getProjectId())
+                        .setCredentials(this.credentialsProvider.getCredentials());
+
+        if (this.universeDomain != null) {
+            storageOptionsBuilder.setUniverseDomain(this.universeDomain);
+        }
+        if (this.host != null) {
+            storageOptionsBuilder.setHost(verifyAndFetchHost(this.host));
+        }
+        return storageOptionsBuilder.build().getService();
+    }
+
+    /**
+     * Verifies and returns host in the `https://${service}.${universeDomain}/` format, following
+     * convention in com.google.cloud.ServiceOptions#getResolvedApiaryHost().
+     *
+     * @param host host provided through `spring.cloud.gcp.storage.host` property
+     * @return host formatted as `https://${service}.${universeDomain}/`
+     */
+    private String verifyAndFetchHost(String host) {
+        URL url;
+        try {
+            url = new URL(host);
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException(
+                    "Invalid host format: "
+                            + host
+                            + ". Please verify that the specified host follows the 'https://${service}.${universeDomain}/' format");
+        }
+        return url.getProtocol() + "://" + url.getAuthority() + "/";
+    }
+}
